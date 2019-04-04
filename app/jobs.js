7f54678a80ec7d70fe3dadd4aaff749900eab6da
m0sk1t <m0sk1t@bk.ru>
2019-04-04 05:38:21 +0300

    Routing and simple job runner

---
+++ app/jobs.js
@@ -0,0 +1,26 @@
+const crypto = require('crypto');
+const router = require('express').Router();
+
+const redisClient = require('./redisClient');
+const runJob = require('./runJob');
+
+router.get('/:company/:tag', (req, res) => {
+  const { company, tag } = req.params;
+  const redisInstance = redisClient.getConn();
+  try {
+    const hash = crypto.createHmac('sha256', 'tsss.. secret').update(company + tag).digest('hex');
+    redisInstance.get(hash, (err, key) => {
+      if (err) return res.status(500).send(`Error while creating job: ${err.message}`);
+      !key && redisInstance.set(hash, 'running');
+      !key && runJob.then((status) => {
+        console.log(status);
+        redisInstance.set(hash, 'done!');
+      });
+      return res.send(`Job status: ${key ? key : 'created'}`);
+    });
+  } catch (err) {
+    return res.status(500).send(`Error while creating job: ${err.message}`);
+  }
+});
+
+module.exports = router;
